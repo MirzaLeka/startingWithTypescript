80fe48a0692a01c5b3ac94827e6a7ef6635f6f6c
mirza.leka <mirza_376@hotmail.com>
2018-06-05 23:50:11 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld() {
+    }
+    HelloWorld.main = function () {
+        console.log("Hello World");
+    };
+    return HelloWorld;
+}());
+HelloWorld.main();
+var myName = "Mirza";
+var myAge = 24;
+var haveGF = false;
+var anything = "whatEver";
+// run auto-compiler: tsc *.ts --watch --target ES5
+// quickly compile: tsc app.ts
---
+++ app.ts
@@ -0,0 +1,19 @@
+
+class HelloWorld {
+
+public static main (): void {
+console.log("Hello World");
+        }
+  }
+
+  HelloWorld.main();
+
+
+var myName : string = "Mirza";
+var myAge : number = 24;
+var haveGF : boolean = false;
+var anything : any = "whatEver";
+
+// run auto-compiler: tsc *.ts --watch --target ES5
+
+// quickly compile: tsc app.ts
