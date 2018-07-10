6e21db2394b7dd6b47684734ba6e4adf537a57d8
Mirza Leka <mirza_376@hotmail.com>
2018-07-10 23:59:09 +0200

    Starting with TS Enums

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-var HelloWorld = /** @class */ (function () {
-    function HelloWorld() {
-    }
-    HelloWorld.main = function () {
-        console.log("Hello World");
-    };
-    return HelloWorld;
-}());
-HelloWorld.main();
+/*
+class HelloWorld {
+
+public static main (): void {
+console.log("Hello World");
+        }
+  }
+*/
+//  HelloWorld.main();
 var myName = "Mirza";
 var myAge = 24;
 var haveGF = false;
-var anything = "whatEver";
+var whatever = 1;
+var myLastName = "Leka";
+whatever = "23";
 // run auto-compiler: tsc *.ts --watch --target ES5
 // quickly compile: tsc app.ts
+// OBJECT
+var student = {
+    Name: "Mirza",
+    Age: 24,
+    Phone: 123321
+};
+// ARRAY
+var studentsList = [
+    {
+        Name: "Ermin",
+        Age: 23,
+        Phone: 471961
+    }
+];
+studentsList.push(student);
+// FOR LOOP
+for (var i = 0; i < studentsList.length; i++) {
+    var element = studentsList[i];
+    console.log("\n  Age: " + element.Age + "\n  Name: " + element.Name + "\n  ");
+}
+// ENUMS
+var languageKnown;
+(function (languageKnown) {
+    languageKnown[languageKnown["JS"] = 0] = "JS";
+    languageKnown[languageKnown["Java"] = 1] = "Java";
+    languageKnown[languageKnown["CSharp"] = 2] = "CSharp";
+    languageKnown[languageKnown["Pascal"] = 3] = "Pascal";
+})(languageKnown || (languageKnown = {}));
+;
--- app.ts
+++ app.ts
@@ -1,19 +1,60 @@
 
+/*
 class HelloWorld {
 
 public static main (): void {
 console.log("Hello World");
         }
   }
+*/
 
-  HelloWorld.main();
+//  HelloWorld.main();
 
 
 var myName : string = "Mirza";
 var myAge : number = 24;
 var haveGF : boolean = false;
-var anything : any = "whatEver";
+var whatever : any = 1;
+var myLastName = "Leka";
+whatever = "23"; 
 
 // run auto-compiler: tsc *.ts --watch --target ES5
 
 // quickly compile: tsc app.ts
+
+// OBJECT
+
+let student = {
+  Name: "Mirza",
+  Age: 24,
+  Phone: 123321
+}
+
+// ARRAY
+
+let studentsList = [
+  {
+    Name: "Ermin",
+    Age: 23,
+    Phone: 471961
+  }
+];
+
+studentsList.push(student);
+
+// FOR LOOP
+
+for (var i = 0; i < studentsList.length; i++) {
+  var element = studentsList[i];
+  console.log(`
+  Age: ${element.Age}
+  Name: ${element.Name}
+  `);
+}
+
+
+// ENUMS
+
+enum languageKnown  {
+  JS, Java, CSharp, Pascal
+};
